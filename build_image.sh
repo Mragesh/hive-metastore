980e179638a54d33b433eef3b570f63a31117f8b
Mragesh Khandelwal <mragesh.khandelwal@gmail.com>
2020-06-03 15:20:32 +0000

    Adding build files for hive metastore

---
+++ build_image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+REPONAME=mragesh
+TAG=hivemetastore
+
+docker build -t $TAG .
+
+# Tag and push to the public docker repository.
+docker tag $TAG $REPONAME/$TAG
+docker push $REPONAME/$TAG
+
+# Update configmaps
+kubectl create configmap metastore-cfg --dry-run --from-file=metastore-site.xml --from-file=core-site.xml -o yaml | kubectl apply -f -
