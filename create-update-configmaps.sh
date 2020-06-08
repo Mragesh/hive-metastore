172c5d55dbe112d2fe774b4ada9b218d20ece547
Mragesh Khandelwal <mragesh.khandelwal@gmail.com>
2020-06-08 18:07:03 +0000

    Created a separate file to create s3 access secret and update configuration

--- create-update-configmaps.sh
+++ create-update-configmaps.sh
@@ -2,4 +2,3 @@
 
 kubectl create configmap metastore-cfg --dry-run --from-file=metastore-site.xml --from-file=core-site.xml -o yaml | kubectl apply -f -
 
-kubectl create secret generic my-s3-keys --from-literal=access-key='myaccesskey' --from-literal=secret-key='mysecretkey'
