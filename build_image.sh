41fe1f78a2795f296e67fd13ecbdadc2db97934a
Mragesh Khandelwal <mragesh.khandelwal@gmail.com>
2020-06-08 17:44:37 +0000

    Removed config setup from build image script, it should run separately

--- build_image.sh
+++ build_image.sh
@@ -9,6 +9,3 @@ docker build -t $TAG .
 # Tag and push to the public docker repository.
 docker tag $TAG $REPONAME/$TAG
 docker push $REPONAME/$TAG
-
-# Update configmaps
-kubectl create configmap metastore-cfg --dry-run --from-file=metastore-site.xml --from-file=core-site.xml -o yaml | kubectl apply -f -
